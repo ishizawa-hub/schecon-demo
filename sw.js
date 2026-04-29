3dd99f2ea4770399ceb8bfbffc0e9fa202264781
ishizawa-hub <ishizawa@epoch-inc.jp>
2026-04-29 13:18:04 +0900

    deploy: 20260429-041701 (cache cleared)

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
  */
 
 // BUILD_VERSION は各デプロイで更新して旧キャッシュを強制破棄する
-const BUILD_VERSION = '20260429-032508';
+const BUILD_VERSION = '20260429-041701';
 const CACHE_VERSION = `schecon-${BUILD_VERSION}`;
 const APP_SHELL = [
   '/schecon-demo/',
